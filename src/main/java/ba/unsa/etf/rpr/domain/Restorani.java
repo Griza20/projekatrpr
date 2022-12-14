45d83c9d38532bab8b7610b7d33bf0bc3c3caf0b
Griza20 <amargrizovic5@gmail.com>
2022-12-14 18:34:04 +0100

    Creating a new bean class Restorani with a method for getting id of a restaurant.

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Restorani.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * Bean for restorani
+ * @author Amar Grizovic
+ */
+public class Restorani {
+    private int idRestorana;
+    private String naziv, vlasnik, lokacija;
+
+    public int getIdRestorana() {
+        return idRestorana;
+    }
+}
