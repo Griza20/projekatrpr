df0fa1c02abea9d3de932c0d1326c184c71dbfd3
Griza20 <amargrizovic5@gmail.com>
2022-12-14 18:15:46 +0100

    Creating a new bean class Narudzbe with a method for getting id of an order.

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Narudzbe.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * Bean for narudzbe
+ * @author Amar Grizovic
+ */
+public class Narudzbe {
+    private int idNarudzbe, idRestorana, idDostavljaca;
+    private String narudzba, vrijemeNarucivanja;
+
+    public int getIdNarudzbe() {
+        return idNarudzbe;
+    }
+}
