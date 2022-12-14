57ff6ae4153eb028952de6381fb3e3fab0cf5a6f
Griza20 <amargrizovic5@gmail.com>
2022-12-14 19:16:07 +0100

    Creating a new Dao for restaurants with a method for getting list of restaurants that have the same manager.

---
+++ src/main/java/ba/unsa/etf/rpr/dao/RestoraniDao.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Restorani;
+
+import java.util.List;
+
+/**
+ * Dao interface for Restorani domain bean
+ * @author Amar Grizovic
+ */
+public interface RestoraniDao extends Dao<Restorani> {
+    /**
+     * Searches for all restaurants that have the same manager
+     * @param vlasnik first and last name of manager
+     * @return List of all restaurants where given parameter is a manager
+     */
+    List<Restorani> searchByManagerName(String vlasnik);
+}
