08f0ced016b28f14a7ea9c3d50fb8811c2e3df2a
Griza20 <amargrizovic5@gmail.com>
2022-12-17 19:16:01 +0100

    Modifying all methods to throw OrderException.

--- src/main/java/ba/unsa/etf/rpr/dao/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
 import java.util.List;
 
 /**
@@ -13,31 +15,31 @@ public interface Dao<Tip> {
      * @param id primary key of entity
      * @return Entity from database
      */
-    Tip getById(int id);
+    Tip getById(int id) throws OrderException;
 
     /**
      * Saves entity into database
      * @param item data that will be saved in database
      * @return saved item
      */
-    Tip add(Tip item);
+    Tip add(Tip item) throws OrderException;
 
     /**
      * Updates already existing entity in database
      * @param item item that will be updated in database
      * @return updated item
      */
-    Tip update(Tip item);
+    Tip update(Tip item) throws OrderException;
 
     /**
      * Deletes entity from database with given id
      * @param id primary key of entity
      */
-    void delete(int id);
+    void delete(int id) throws OrderException;
 
     /**
      * Shows list of all entities from database
      * @return list of all entities
      */
-    List<Tip> getAll();
+    List<Tip> getAll() throws OrderException;
 }
