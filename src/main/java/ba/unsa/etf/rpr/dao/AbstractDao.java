99d378506554e1128a2c2d0eb0421c2f951fa1b3
Griza20 <amargrizovic5@gmail.com>
2022-12-17 19:12:59 +0100

    Adding a new AbstractDao class with abstract contructor for connecting on database.

---
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -0,0 +1,30 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Idable;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+/**
+ * Abstract class that implements core DAO CRUD methods for every entity
+ * @author Amar Grizovic
+ */
+public abstract class AbstractDao<T extends Idable> implements Dao<T>{
+    private Connection connection;
+    private String imeTabele;
+
+    public AbstractDao(String imeTabele){
+        try{
+            this.imeTabele=imeTabele;
+            FileReader reader = new FileReader("baza.properties");
+            Properties property = new Properties();
+            property.load(reader);
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Baza",property.getProperty("username"),property.getProperty("password"));
+        }catch(Exception e){
+            System.out.println("Nije uspostavljena konekcija sa bazom.");
+            e.printStackTrace();
+        }
+    }
+}
