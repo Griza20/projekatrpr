61c46c6afaa0d4ebe532aa7541001e948b9a455b
Griza20 <amargrizovic5@gmail.com>
2022-12-17 19:26:01 +0100

    Implementing prepareInsertParts method that can be used for preparing parts for inserting into database.

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -5,6 +5,7 @@ import ba.unsa.etf.rpr.exceptions.OrderException;
 
 import java.io.FileReader;
 import java.sql.*;
+import java.util.AbstractMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -48,5 +49,28 @@ public abstract class AbstractDao<T extends Idable> implements Dao<T>{
         } catch (SQLException e) {
             throw new OrderException(e.getMessage(), e);
         }
+    }
+
+    /**
+     * Accepts KV storage of column names and return CSV of columns and question marks for insert statement
+     * Example: (id, name, date) ?,?,?
+     * @author Amar Grizovic
+     */
+    private Map.Entry<String, String> prepareInsertParts(Map<String, Object> row){
+        StringBuilder columns = new StringBuilder();
+        StringBuilder questions = new StringBuilder();
+
+        int counter = 0;
+        for (Map.Entry<String, Object> entry: row.entrySet()) {
+            counter++;
+            if (entry.getKey().equals("id")) continue;
+            columns.append(entry.getKey());
+            questions.append("?");
+            if (row.size() != counter) {
+                columns.append(",");
+                questions.append(",");
+            }
+        }
+        return new AbstractMap.SimpleEntry<String,String>(columns.toString(), questions.toString());
     }
 }
