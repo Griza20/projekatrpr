04737dbc1883890e6cdc98bc1530d1f28c331c37
Griza20 <amargrizovic5@gmail.com>
2022-12-25 11:48:16 +0100

    Creating new class which represents business logic layer for bean class Restorani.

---
+++ src/main/java/ba/unsa/etf/rpr/business/RestoraniManager.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.business;
+
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Restorani;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+
+/**
+ * Business Logic Layer for Restorani
+ * @author Amar Grizovic
+ */
+public class RestoraniManager {
+    public Restorani getById(int idRestorana) throws OrderException {
+        return DaoFactory.restoraniDao().getById(idRestorana);
+    }
+}
