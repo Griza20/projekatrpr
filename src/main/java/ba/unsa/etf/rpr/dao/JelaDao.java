a54319ed9237a7fb231bbc7917a4b19b17e8ad44
Griza20 <amargrizovic5@gmail.com>
2022-12-26 14:13:38 +0100

    Creating JelaDao interface for domain bean class Jela.

---
+++ src/main/java/ba/unsa/etf/rpr/dao/JelaDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Jela;
+
+/**
+ * Dao interface for Jela domain bean
+ * @author Amar Grizovic
+ */
+public interface JelaDao extends Dao<Jela>{
+}
