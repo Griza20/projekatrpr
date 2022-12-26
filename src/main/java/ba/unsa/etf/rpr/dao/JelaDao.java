aff47bb5e5701cbdb44fdde91849a07eeb24854a
Griza20 <amargrizovic5@gmail.com>
2022-12-26 18:51:25 +0100

    Adding new abstract method in interface that must be implemented for getting all meals from restaurant with id given as parameter.

--- src/main/java/ba/unsa/etf/rpr/dao/JelaDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/JelaDao.java
@@ -1,10 +1,18 @@
 package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Jela;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import java.util.List;
 
 /**
  * Dao interface for Jela domain bean
  * @author Amar Grizovic
  */
 public interface JelaDao extends Dao<Jela>{
+    /**
+     * Gets all meals from a restaurant whose id is given as parameter
+     * @param idRestorana unique id of a restaurant
+     * @return List of all meals from a restaurant
+     */
+    List<Jela> getAllMealsFromRestaurant(int idRestorana) throws OrderException;
 }
