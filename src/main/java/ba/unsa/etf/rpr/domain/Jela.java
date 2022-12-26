719ae5dd908e56c7af0f7537a1af3770bcae6a1e
Griza20 <amargrizovic5@gmail.com>
2022-12-26 14:06:03 +0100

    Creating new bean class Jela for presenting food in restaurant on main screen.

---
+++ src/main/java/ba/unsa/etf/rpr/domain/Jela.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+public class Jela {
+    private int idJela, idRestorana, cijena;
+    private String jelo;
+
+    public int getCijena() {
+        return cijena;
+    }
+}
