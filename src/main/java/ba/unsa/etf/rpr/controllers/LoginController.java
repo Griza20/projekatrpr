2ea6a7b70f01acb825c071f7c52bc9c51d1dad05
Griza20 <amargrizovic5@gmail.com>
2023-02-07 21:37:21 +0100

    Adding some minor changes in LoginController such as Alert message if data isn't correct.

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rpr.controllers;
 
-import ba.unsa.etf.rpr.exceptions.OrderException;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -8,18 +7,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Properties;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
@@ -56,7 +54,7 @@ public class LoginController {
             FileReader reader = new FileReader("baza.properties");
             Properties property = new Properties();
             property.load(reader);
-            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Baza",property.getProperty("username"),property.getProperty("password"));
+            this.connection = DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Baza?sessionVariables=WAIT_TIMEOUT=28800",property.getProperty("username"),property.getProperty("password"));
         }catch(Exception e){
             System.out.println("Nije uspostavljena konekcija sa bazom.");
             e.printStackTrace();
@@ -84,7 +82,7 @@ public class LoginController {
                     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/home-layout.fxml"));
                     loader.load();
                     HomeController homeController = loader.getController();
-                    myStage.setTitle("Home Screen");
+                    myStage.setTitle("Sistem naručivanja v1.0");
                     myStage.setScene(new Scene((Parent) loader.getRoot(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
                     myStage.setResizable(false);
                     myStage.show();
@@ -95,7 +93,11 @@ public class LoginController {
                 }
             }
             else{
-                System.out.println("Pogresan login");
+                Alert poruka = new Alert(Alert.AlertType.INFORMATION);
+                poruka.setTitle("Poruka");
+                poruka.setContentText("Pogrešan unos podataka.");
+                poruka.showAndWait();
+                userField.requestFocus();
             }
         }catch (SQLException e){
             e.printStackTrace();
