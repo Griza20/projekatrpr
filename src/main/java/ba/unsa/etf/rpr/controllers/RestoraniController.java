aba9046521bb6d91911c08b3a0a40f861120cd8f
Griza20 <amargrizovic5@gmail.com>
2023-02-07 21:24:35 +0100

    Adding dodajButton that adds new restaurant in database if fields aren't empty.

--- src/main/java/ba/unsa/etf/rpr/controllers/RestoraniController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RestoraniController.java
@@ -37,4 +37,33 @@ public class RestoraniController {
         nazivRestorana.getStyleClass().removeAll("poljeNijeIspravno");
         lokacijaRestorana.getStyleClass().removeAll("poljeNijeIspravno");
     }
+
+    public void dodajButton(ActionEvent actionEvent) throws OrderException {
+        boolean validacija=true;
+        imeVlasnika.getStyleClass().removeAll("poljeNijeIspravno");
+        nazivRestorana.getStyleClass().removeAll("poljeNijeIspravno");
+        lokacijaRestorana.getStyleClass().removeAll("poljeNijeIspravno");
+        if(imeVlasnika.getText().equals("")){
+            imeVlasnika.getStyleClass().add("poljeNijeIspravno");
+            validacija=false;
+        }
+        if(nazivRestorana.getText().equals("")){
+            nazivRestorana.getStyleClass().add("poljeNijeIspravno");
+            validacija=false;
+        }
+        if(lokacijaRestorana.getText().equals("")){
+            lokacijaRestorana.getStyleClass().add("poljeNijeIspravno");
+            validacija=false;
+        }
+        if(validacija){
+            Restorani resto = new Restorani();
+            resto.setNaziv(nazivRestorana.getText());
+            resto.setVlasnik(imeVlasnika.getText());
+            resto.setLokacija(lokacijaRestorana.getText());
+            restoraniManager.add(resto);
+            Node n = (Node) actionEvent.getSource();
+            Stage stage = (Stage) n.getScene().getWindow();
+            stage.close();
+        }
+    }
 }
