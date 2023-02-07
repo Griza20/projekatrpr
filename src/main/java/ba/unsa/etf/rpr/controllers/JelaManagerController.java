687327ce6f33f2640b07fb2212638901d78badb1
Griza20 <amargrizovic5@gmail.com>
2023-02-07 22:23:57 +0100

    Creating a new class JelaManagerController for manipulation with editing or deleting an existing meal from a specific restaurant, initially having JeloModel class inside its body and constructor without parameters.

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/JelaManagerController.java
@@ -0,0 +1,65 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.JelaManager;
+import ba.unsa.etf.rpr.business.RestoraniManager;
+import ba.unsa.etf.rpr.domain.Jela;
+import ba.unsa.etf.rpr.domain.Restorani;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.util.List;
+
+public class JelaManagerController {
+    public TextField jeloField, opisField;
+    public ChoiceBox<Restorani> restoChoice;
+    public ChoiceBox<Jela> jelaChoice;
+    public Spinner<Double> cijenaField;
+    private JeloModel jeloModel;
+    private JelaManager jelaManager;
+    private RestoraniManager restoraniManager;
+    private ObservableList<Restorani> restorani;
+    private ObservableList<Jela> listaJela = FXCollections.observableArrayList();
+    public Button izlazButton;
+    public JelaManagerController() {
+        jelaManager = new JelaManager();
+        restoraniManager = new RestoraniManager();
+        jeloModel = new JeloModel();
+        try {
+            restorani = FXCollections.observableArrayList(restoraniManager.getAll());
+        } catch (OrderException e) {
+            System.out.println("Problem sa getAll metodom restoraniManager");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public class JeloModel{
+        public Integer id, restoId;
+        public SimpleStringProperty jelo = new SimpleStringProperty();
+        public SimpleStringProperty opis = new SimpleStringProperty();
+        public DoubleProperty cijena = new SimpleDoubleProperty();
+        public JeloModel(){}
+        public void fromJelo(Jela j){
+            this.id = j.getId();
+            this.jelo = new SimpleStringProperty(j.getJelo());
+            this.opis = new SimpleStringProperty(j.getOpis());
+            this.cijena = new SimpleDoubleProperty(j.getCijena());
+        }
+        public Jela toJelo() {
+            Jela j = new Jela();
+            j.setId(jeloModel.id);
+            j.setJelo(jelo.get());
+            j.setOpis(opis.get());
+            j.setCijena(jeloModel.cijena.getValue());
+            j.setIdRestorana(jeloModel.restoId);
+            return j;
+        }
+    }
+}
