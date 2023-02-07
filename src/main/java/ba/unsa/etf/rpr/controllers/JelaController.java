33ad5f30fd5b5a7d33ea8858f6ae20b57338ab18
Griza20 <amargrizovic5@gmail.com>
2023-02-07 21:57:24 +0100

    Creating new class JelaController for manipulation with adding a new meal in a specific restaurant initially having constructor with idRestaurant parameter and private method for validation of fields.

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/JelaController.java
@@ -0,0 +1,39 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.JelaManager;
+import ba.unsa.etf.rpr.domain.Jela;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+
+public class JelaController {
+    public Button izlazButton;
+    public TextField opisField;
+    public TextField nazivField;
+    public Spinner<Double> cijenaSpinner;
+    private JelaManager jelaManager;
+    private int idResto;
+    private boolean validacija(){
+        boolean v=true;
+        if(nazivField.getText().isEmpty()){
+            v=false;
+            nazivField.getStyleClass().add("poljeNijeIspravno");
+        }
+        if(opisField.getText().isEmpty()){
+            v=false;
+            opisField.getStyleClass().add("poljeNijeIspravno");
+        }
+        return v;
+    }
+
+    public JelaController(int idRestorana){
+        jelaManager = new JelaManager();
+        idResto=idRestorana;
+    }
+}
