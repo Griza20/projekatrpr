31b8fd9571c14ab28bf2fd011d371a9a870444a1
Griza20 <amargrizovic5@gmail.com>
2023-02-07 21:21:33 +0100

    Creating new class RestoraniController to manipulate with adding new restaurant, initially having constructor without parameters and javafx atributes.

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/RestoraniController.java
@@ -0,0 +1,33 @@
+package ba.unsa.etf.rpr.controllers;
+
+import ba.unsa.etf.rpr.business.RestoraniManager;
+import ba.unsa.etf.rpr.dao.RestoraniDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.Restorani;
+import ba.unsa.etf.rpr.exceptions.OrderException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class RestoraniController {
+    public TextField lokacijaRestorana;
+    public TextField nazivRestorana;
+    public TextField imeVlasnika;
+    public Button dodajRestoran;
+    public Button izlaz;
+    private Connection connection;
+    RestoraniManager restoraniManager;
+
+    public RestoraniController(){
+        restoraniManager = new RestoraniManager();
+    }
+}
